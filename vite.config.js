93cb11ef1a2aca5bd736d5c784c95fdf31d65bc0
Rishi <smrishin@gmail.com>
2025-06-26 01:01:33 -0700

    updated deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +16,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")]
+      plugins: [tailwindcss, autoprefixer]
     }
   }
 });
